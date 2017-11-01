43f9c98fc6662c1320d934cfdd949060d06724aa
Unknown <mrshelemba@gmail.com>
2017-11-01 22:24:45 +0200

    Виконав завдання 3.2.

--- lab03_3_main.m
+++ lab03_3_main.m
@@ -48,12 +48,12 @@ r1 = 0.7;
 b1 = [1 0 -1];          
 a1 = 1-2*r1*cos(phi)*r1^2;     
 figure(3)
-[h,f] = freqz(b1, a1, n);
-mag2 = abs(h);
-phase = angle(h)*180/pi;
-subplot(2, 1, 1); plot(f/(2*pi)*fs, mag2), grid on;
+[h1,f1] = freqz(b1, a1, n);
+mag2 = abs(h1);
+phase2 = angle(h1)*180/pi;
+subplot(2, 1, 1); plot(f1/(2*pi)*fs, mag2), grid on;
 title('АЧХ фільтру при r = 0.7'); ylabel('Підсилення');
-subplot(2, 1, 2); plot(f/(2*pi)*fs, unwrap(phase)), grid on;
+subplot(2, 1, 2); plot(f1/(2*pi)*fs, unwrap(phase2)), grid on;
 title('ФЧХ фільтру при r = 0.7'); xlabel('Частота'); ylabel('Фаза');
 
 % Обчислення нулів та полюсів фільтру
@@ -69,12 +69,12 @@ r2 = 0.9;
 b2 = [1 0 -1];          
 a2 = 1-2*r2*cos(phi)*r2^2;     
 figure(5)
-[h,f] = freqz(b2, a2, n);
-mag3 = abs(h);
-phase = angle(h)*180/pi;
-subplot(2, 1, 1); plot(f/(2*pi)*fs, mag3), grid on;
+[h2,f2] = freqz(b2, a2, n);
+mag3 = abs(h2);
+phase3 = angle(h2)*180/pi;
+subplot(2, 1, 1); plot(f2/(2*pi)*fs, mag3), grid on;
 title('АЧХ фільтру при r = 0.9'); ylabel('Підсилення');
-subplot(2, 1, 2); plot(f/(2*pi)*fs, unwrap(phase)), grid on;
+subplot(2, 1, 2); plot(f2/(2*pi)*fs, unwrap(phase3)), grid on;
 title('ФЧХ фільтру при r = 0.9'); xlabel('Частота'); ylabel('Фаза');
 
 % Обчислення нулів та полюсів фільтру
@@ -87,17 +87,16 @@ z2 = zplane(b2);
 
 %=== Завдання #1.4 ===
 %Побудува графіків перехідних процесів
-n = 5;
-[h,t] = stepz(b1, a1, n, fs);
+n2 = 5;
+[h3,t3] = stepz(b1, a1, n2, fs);
 figure(7)
-plot (t,h);
-[h,t] = stepz(b2, a2, n, fs);
+plot (t3, h3);
+[h3,t3] = stepz(b2, a2, n2, fs);
 figure(8)
-plot (t,h);
+plot (t3, h3);
 
 %=== Завдання #2.1 ===
-% Фільтрація ЕКГ при r = 0.6
-fs = 200; 
+% Фільтрація ЕКГ при r = 0.6 
 r3 = 0.6;
 b3 = [1 0 -1];          
 a3 = 1-2*r3*cos(phi)*r3^2; 
@@ -118,7 +117,6 @@ xlabel('Відліки'); ylabel('Амплітуда');
 %=== Завдання #2.2 ===
 % Фільтрація ЕКГ при r = 0.7 та r = 0.8
 % r = 0.7
-fs = 200; 
 r4 = 0.7;
 b4 = [1 0 -1];          
 a4 = 1-2*r4*cos(phi)*r4^2; 
@@ -137,7 +135,6 @@ xlim([0 2]);
 xlabel('Відліки'); ylabel('Амплітуда');
 
 % r = 0.8
-fs = 200; 
 r5 = 0.8;
 b5 = [1 0 -1];          
 a5 = 1-2*r5*cos(phi)*r5^2; 
@@ -168,7 +165,6 @@ xlabel('Відліки'); ylabel('Амплітуда');
 
 %=== Завдання #3.1 ===
 % Дослідження властивостей режекторного НІХ-фільтру
-fs = 200;
 r6 = 0.8;
 phi1 = 110*pi/180;
 phi2 = 130*pi/180; 
@@ -177,3 +173,22 @@ ax2 = [1-2*r6*cos(phi2)*r6^2];
 a6 = conv(ax, ax2);
 b6 = [1 1 1]; 
 H1 = filt(b6, a6) % передавальна функція
+
+%=== Завдання #3.2 ===
+% АЧХ та ФЧХ смугового режекторного НІХ-фільтру
+figure(12)
+[h4, f4] = freqz(a6, b6, n);
+mag4 = abs(h4);
+phase4 = angle(h4)*180/pi;
+subplot(2, 1, 1); plot(f4/(2*pi)*fs, mag4), grid on;
+title ('АЧХ фільтру'); ylabel ('Підсилення');
+subplot(2, 1, 2); plot(f4/(2*pi)*fs, unwrap(phase4)), grid on;
+title('ФЧХ фільтру'); xlabel('Частота'); ylabel('Фаза');
+
+% Обчислення нулів та полюсів фільтру
+x3 = roots (b)
+y3 = poly (a)
+ 
+% Карта нулів та полюсів фільтру
+figure(13)
+z3 = zplane(b,a);
