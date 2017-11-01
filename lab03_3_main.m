143830d83a2da3cbc82685a1cfeba7bbf3d16f10
Unknown <mrshelemba@gmail.com>
2017-11-01 21:44:00 +0200

    Виконав завдання 2.1-2.2.

--- lab03_3_main.m
+++ lab03_3_main.m
@@ -31,7 +31,7 @@ mag = abs(h);
 phase = angle(h)*180/pi;
 subplot(2, 1, 1); plot(f/(2*pi)*fs, mag), grid on;
 title('АЧХ смугового фільтру 2-го порядку'); ylabel('Підсилення');
-subplot(2, 1, 2); plot(f/(2*pi)*fs,unwrap(phase)), grid on;
+subplot(2, 1, 2); plot(f/(2*pi)*fs, unwrap(phase)), grid on;
 title('ФЧХ смугового фільтру 2-го порядку'); xlabel('Частота'); ylabel('Фаза');
 
 % Обчислення нулів та полюсів фільтру
@@ -44,16 +44,16 @@ z = zplane(b);
 
 %=== Завдання #1.3 ===
 % r = 0.7
-r = 0.7;
+r1 = 0.7;
 b1 = [1 0 -1];          
-a1 = 1-2*r*cos(phi)*r^2;     
+a1 = 1-2*r1*cos(phi)*r1^2;     
 figure(3)
-[h,f] = freqz(b1,a1,n);
+[h,f] = freqz(b1, a1, n);
 mag2 = abs(h);
 phase = angle(h)*180/pi;
 subplot(2, 1, 1); plot(f/(2*pi)*fs, mag2), grid on;
 title('АЧХ фільтру'); ylabel('Підсилення');
-subplot(2, 1, 2); plot(f/(2*pi)*fs,unwrap(phase)), grid on;
+subplot(2, 1, 2); plot(f/(2*pi)*fs, unwrap(phase)), grid on;
 title('ФЧХ фільтру'); xlabel('Частота'); ylabel('Фаза');
 
 % Обчислення нулів та полюсів фільтру
@@ -65,16 +65,16 @@ figure(4)
 z1 = zplane(b1);
 
 % r = 0.9
-r = 0.9;
+r2 = 0.9;
 b2 = [1 0 -1];          
-a2 = 1-2*r*cos(phi)*r^2;     
+a2 = 1-2*r2*cos(phi)*r2^2;     
 figure(5)
-[h,f] = freqz(b2,a2,n);
+[h,f] = freqz(b2, a2, n);
 mag3 = abs(h);
 phase = angle(h)*180/pi;
 subplot(2, 1, 1); plot(f/(2*pi)*fs, mag3), grid on;
 title('АЧХ фільтру'); ylabel('Підсилення');
-subplot(2, 1, 2); plot(f/(2*pi)*fs,unwrap(phase)), grid on;
+subplot(2, 1, 2); plot(f/(2*pi)*fs, unwrap(phase)), grid on;
 title('ФЧХ фільтру'); xlabel('Частота'); ylabel('Фаза');
 
 % Обчислення нулів та полюсів фільтру
@@ -94,3 +94,83 @@ plot (t,h);
 [h,t] = stepz(b2, a2, n, fs)
 figure(8)
 plot (t,h);
+
+%=== Завдання #2.1 ===
+% Фільтрація ЕКГ при r = 0.6
+fs = 200; 
+r3 = 0.6;
+b3 = [1 0 -1];          
+a3 = 1-2*r3*cos(phi)*r3^2; 
+ecg = load('ecg105.txt'); % сигнал ЕКГ
+ecg1 = detrend(ecg);
+ecgf1 = filter(b3, a3, ecg1);
+t1 = (0:length(ecgf1)-1)/fs;
+figure(9)
+[h,f] = freqz(b3, a3, n);
+mag3 = abs(h);
+phase = angle(h)*180/pi;
+subplot (2, 1, 1); plot(t1, ecg1), grid on;
+title('Нефільтрований сигнал');
+xlim([0 2]);
+ylabel('Амплітуда');
+subplot (2, 1, 2); plot (t1, ecgf1); grid on;
+title('Відфільтрований сигнал');
+xlim([0 2]);
+xlabel('Відліки'); ylabel('Амплітуда');
+
+%=== Завдання #2.2 ===
+% Фільтрація ЕКГ при r = 0.7 та r = 0.8
+% r = 0.7
+fs = 200; 
+r4 = 0.7;
+b4 = [1 0 -1];          
+a4 = 1-2*r4*cos(phi)*r4^2; 
+ecg = load('ecg105.txt'); % сигнал ЕКГ
+ecg2 = detrend(ecg);
+ecgf2 = filter(b4, a4, ecg2);
+t2 = (0:length(ecgf2)-1)/fs;
+figure(10)
+[h,f] = freqz(b4, a4, n);
+mag4 = abs(h);
+phase = angle(h)*180/pi;
+subplot (2, 1, 1); plot(t2, ecg2), grid on;
+title('Нефільтрований сигнал');
+xlim([0 2]);
+ylabel('Амплітуда');
+subplot (2, 1, 2); plot(t2, ecgf2); grid on;
+title('Відфільтрований сигнал');
+xlim([0 2]);
+xlabel('Відліки'); ylabel('Амплітуда');
+
+% r = 0.8
+fs = 200; 
+r5 = 0.8;
+b3 = [1 0 -1];          
+a3 = 1-2*r5*cos(phi)*r5^2; 
+ecg = load('ecg105.txt'); % сигнал ЕКГ
+ecg3 = detrend(ecg);
+ecgf3 = filter(b3, a3, ecg3);
+t3 = (0:length(ecgf3)-1)/fs;
+figure(11)
+[h,f] = freqz(b3, a3, n);
+mag3 = abs(h);
+phase = angle(h)*180/pi;
+subplot (2, 1, 1); plot(t3, ecg3), grid on;
+title('Нефільтрований сигнал');
+xlim([0 2]);
+ylabel('Амплітуда');
+subplot (2, 1, 2); plot(t3, ecgf3); grid on;
+title('Відфільтрований сигнал');
+xlim([0 2]);
+xlabel('Відліки'); ylabel('Амплітуда');
+
+%Графіки результатів фільтрації
+figure(12)
+subplot(2, 1, 1); plot(t2, ecgf2); grid on;
+title('Відфільтрований сигнал при r = 0.7');
+xlim([0 2]);
+ylabel('Амплітуда');
+subplot (2, 1, 2); plot (t3, ecgf3); grid on;
+title('Відфільтрований сигнал при r = 0.8');
+xlim([0 2]);
+xlabel('Відліки'); ylabel('Амплітуда');
