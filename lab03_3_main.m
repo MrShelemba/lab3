f32b88e4a8f6ce4564f1a6dc31bb09b039b6db37
Unknown <mrshelemba@gmail.com>
2017-11-02 21:20:32 +0200

    Виконав завдання 4.1.

--- lab03_3_main.m
+++ lab03_3_main.m
@@ -105,10 +105,10 @@ title('Карта нулів та полюсів смугового фільтр
 n2 = 5;
 [h3, t3] = stepz(b1, a1, n2, fs);
 figure(7)
-plot(t3, h3);
+stepz(t3, h3);
 [h3, t3] = stepz(b2, a2, n2, fs);
 figure(8)
-plot(t3, h3);
+stepz(t3, h3);
 
 %=== Завдання #2.1 ===
 % Фільтрація ЕКГ при r = 0.6 
@@ -120,11 +120,11 @@ ecgd1 = detrend(ecg);
 ecgf1 = filter(b3, a3, ecgd1);
 t1 = (0:length(ecgf1)-1)/fs;
 figure(9)
-subplot(2, 1, 1); plot(t1, ecgd1), grid on;
-title('Нефільтрований сигнал');
+subplot(2, 1, 1); plot(t1, ecg), grid on;
+title('Сигнал з шумом');
 xlim([0 2]);
 ylabel('Амплітуда');
-subplot(2, 1, 2); plot(t1, ecgf1); grid on;
+subplot(2, 1, 2); plot(t1, ecgd1); grid on;
 title('Відфільтрований сигнал при r = 0.6');
 xlim([0 2]);
 xlabel('Відліки'); ylabel('Амплітуда');
@@ -139,7 +139,7 @@ ecgf2 = filter(b4, a4, ecgd1);
 t2 = (0:length(ecgf2)-1)/fs;
 figure(10)
 subplot(2, 1, 1); plot(t2, ecgd1), grid on;
-title('Нефільтрований сигнал');
+title('Сигнал з шумом');
 xlim([0 2]);
 ylabel('Амплітуда');
 subplot(2, 1, 2); plot(t2, ecgf2), grid on;
@@ -155,7 +155,7 @@ ecgf3 = filter(b5, a5, ecgd1);
 t3 = (0:length(ecgf3)-1)/fs;
 figure(11)
 subplot(2, 1, 1); plot(t3, ecgd1), grid on;
-title('Нефільтрований сигнал');
+title('Сигнал з шумом');
 xlim([0 2]);
 ylabel('Амплітуда');
 subplot(2, 1, 2); plot(t3, ecgf3), grid on;
@@ -237,10 +237,54 @@ ecgf4 = filter(b6, a6, ecgd2);
 t4 = (0:length(ecgf4)-1)/fs;
 figure(15)   
 subplot(2, 1, 1); plot(t4, ecgd2), grid on;
-title('Нефільтрований сигнал з мережевою перешкодою 60 Гц');
+title('Сигнал з мережевою перешкодою 60 Гц');
 xlim([1 2]);
 ylabel('Амплітуда');
 subplot(2, 1, 2); plot (t4, ecgf4), grid on;
 title('Відфільтрований сигнал');
 xlim([1 2]);
 xlabel('Відліки'); ylabel('Амплітуда');
+
+%=== Завдання #4.1 ===
+% АЧХ і ФЧХ цифрових інтеграторів
+% Інтегрування методом прямокутників 
+fs = 300; % частота дискретизації
+T = 1/fs; 
+bi1 = T; % чисельник ПФ
+ai1 = [1 -1]; % знаменник ПФ
+
+% Інтегрування методом трапецій.
+bi2 = [1 1]*T/2;
+ai2 = [1 -1];
+
+% Інтегрування методом парабол (Сімпсона).
+bi3 = [1 4 1]*T/3;
+ai3 = [1 0 -1];
+
+[hi1, wi1] = freqz(bi1, ai1, n);
+magi1 = abs(hi1);
+phasei1 = angle(hi1)*180/pi;
+
+[hi2, wi2] = freqz(bi2, ai2, n);
+magi2 = abs(hi2);
+phasei2 = angle(hi2)*180/pi;
+
+[hi3, wi3] = freqz(bi3, ai3, n);
+magi3 = abs(hi3);
+phasei3 = angle(hi3)*180/pi;
+
+figure(16)
+subplot(3, 1, 1); plot(wi1/(2*pi)*fs, magi1), grid on;
+title('АЧХ інтегратора (метод прямокутників)'); ylabel('Підсилення');
+subplot(3, 1, 2); plot(wi2/(2*pi)*fs, magi2), grid on;
+title ('АЧХ інтегратора (метод трапецій)'); ylabel ('Підсилення');
+subplot(3, 1, 3); plot(wi3/(2*pi)*fs, magi3), grid on;
+title ('АЧХ інтегратора (метод парабол)'); xlabel ('Частота'); ylabel ('Підсилення');
+
+figure(17)
+subplot(3, 1, 1); plot(wi1/(2*pi)*fs, unwrap(phasei1)), grid on;
+title('ФЧХ інтегратора (метод прямокутників)'); ylabel('Фаза');
+subplot(3, 1, 2); plot(wi2/(2*pi)*fs, unwrap(phasei2)), grid on;
+title('ФЧХ інтегратора (метод трапецій)'); ylabel('Фаза');
+subplot(3, 1, 3); plot(wi3/(2*pi)*fs, unwrap(phasei3)), grid on;
+title('ФЧХ інтегратора (метод парабол)'); xlabel('Частота'); ylabel('Фаза');
